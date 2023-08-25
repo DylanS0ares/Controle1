f2afdc4bf0a3d3f90792151f96c670bffc02bd02
Dylan <bdylansoares@gmail.com>
2023-08-25 12:50:55 -0300

    atualização número real

--- fracoes_parciais.py
+++ fracoes_parciais.py
@@ -7,7 +7,10 @@ from scipy import signal
 num = [2,5,3,6]
 den = [1,6,11,6]
 r,p,k = signal.residue(num,den)
-print('Resíduos:',r)
-print('Polos: ',p)
-print('Termo direto:',k)
+for i in range(len(r)):
+    print('Resíduos:{:.1f}'.format(r[i]))
+for i in range(len(p)): 
+    print('Polos:{:.1f} '.format(p[i]))
+for i in range(len(k)):
+    print('Termo direto: {:.1f}'.format(k[i]))
 
